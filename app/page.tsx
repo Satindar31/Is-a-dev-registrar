a0dfd57c4d4977f15340dff0f0542b756d26dc13
Satindar <ibrahimasif711@outlook.com>
2024-09-21 15:51:42 +0530

    shittier homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-// "use client"
 
-// import { HomeInput } from "@/components/home/input";
 import Hero from "@/components/home/hero";
 import { HomeInput } from "@/components/home/input";
+import { Boxes } from "@/components/ui/background-boxes";
 import { HeroHighlight } from "@/components/ui/hero-highlight";
 
 export default function Home() {
   return (
-    <>
-      <HeroHighlight>
+    <div className="h-screen p-8">
         <Hero />
+      <Boxes />
         <HomeInput />
-      </HeroHighlight>
-    </>
+    </div>
   );
 }
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        className="text-2xlmd:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
         Welcome to the{" "}
         <Highlight className="text-black dark:text-white">
